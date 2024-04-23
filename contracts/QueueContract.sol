59b63f4bc3693dc54d431d1cc2658f9675540394
shuklapalak <shuklaapalak@gmail.com>
2024-04-23 20:39:19 +0530

    Setup blockchain project

--- contracts/QueueContract.sol
+++ contracts/QueueContract.sol
@@ -2,36 +2,36 @@
 pragma solidity >=0.6.12 <0.9.0;
 
 contract QueueContract {
-  uint256[] arr; // array
+  uint256[] queue; // array
   uint32 constant public SIZE = 10; // size of array
 
   // Push value at index 0
   function push(uint val) public {
-    require(arr.length != SIZE, "Array is at maximum capacity");
-    arr.push(0);
-    for (uint256 i = arr.length - 1; i > 0; i--) {
-      arr[i] = arr[i-1];
+    require(queue.length != SIZE, "Array is at maximum capacity");
+    queue.push(0);
+    for (uint256 i = queue.length - 1; i > 0; i--) {
+      queue[i] = queue[i-1];
     }
-    arr[0] = val;
+    queue[0] = val;
   }
 
   // Remove value
   function pop() public {
-    require(arr.length != 0, "Array is empty");
-    for (uint256 i = 0; i < arr.length - 1; i++) {
-      arr[i] = arr[i + 1];
+    require(queue.length != 0, "Array is empty");
+    for (uint256 i = 0; i < queue.length - 1; i++) {
+      queue[i] = queue[i + 1];
     }
-    arr.pop();
+    queue.pop();
   }
 
   // Get value of array
   function get() public view returns (uint[] memory) {
-    return arr;
+    return queue;
   }
 
   // Modify the value at a specific index.
   function update(uint val, uint index) public {
-    require(index > arr.length, "Invalid Index");
-    arr[index] = val;
+    require(index > queue.length, "Invalid Index");
+    queue[index] = val;
   }
 }
